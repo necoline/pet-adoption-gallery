046decfd938c558d65985a03d727dcfb8d93f324
Necoline <hnhubner@gmail.com>
2019-06-09 08:32:28 -0700

    cleans code

--- content.js
+++ content.js
@@ -26,16 +26,11 @@ snippets.onLoad(function() {
       const dogData = JSON.parse(response.target.response);
       return dogData.dogs;
     })
-    // fetch('https://picsum.photos/v2/list?limit=30')
-    //   .then(function(response) {
-    //     return response.json();
-    //   })
     .then(function(jsonData) {
-      console.log("jsonData", jsonData);
       createImages(jsonData);
     })
     .catch(err => {
-      console.log(err);
+      console.error(err);
     });
 
   function createImages(imgs) {
@@ -51,7 +46,7 @@ snippets.onLoad(function() {
     lazyTargets.forEach(lazyLoad);
   }
 
-  // The lazy loading observer
+  // The lazy load observer
   function lazyLoad(target) {
     const obs = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
